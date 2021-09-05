4625f8ec78f019761ef4286a0413f16e03bf231f
Yasen Kostov <qskatak04@gmail.com>
2021-09-05 17:16:36 +0300

    add form and handle event for assign_match in heex

--- lib/tennis_phx_web/live/tour_live.ex
+++ lib/tennis_phx_web/live/tour_live.ex
@@ -16,13 +16,15 @@ defmodule TennisPhxWeb.TourLive do
   def mount(params, _, socket) do
     tour = Events.get_tour!(params["id"])
     players = Participants.list_players()
+    players_for_tour = tour.players |> Repo.preload(:tours)
     tour_players = Events.tour_players(tour)
                    |>Enum.map(fn(x) -> x.player_id end)
     socket = assign(
         socket,
         tour: tour,
         players: players,
-        tour_players: tour_players
+        tour_players: tour_players,
+        players_for_tour: players_for_tour
       )
     {:ok, socket}
   end
@@ -30,11 +32,14 @@ defmodule TennisPhxWeb.TourLive do
 
   def handle_event("toggle_check", %{"player-id" => player_id}, socket) do
     tour = socket.assigns[:tour]
-              |> Repo.preload(:players)
+           |> Repo.preload(:players)
+    players_for_tour = tour.players |> Repo.preload(:tours)
     Events.toggle_tour_players(tour, player_id)
     tour_players = Events.tour_players(tour)
                    |>Enum.map(fn(x) -> x.player_id end)
                    {:noreply, assign(socket, :tour_players, tour_players)}
+
+
   end
 
 
@@ -43,6 +48,18 @@ defmodule TennisPhxWeb.TourLive do
     tour = socket.assigns[:tour]
            |> Repo.preload(:players)
     Events.assign_player_points(tour, player_id, points_for_player)
+    {:noreply, socket}
+  end
+
+  def handle_event("assign_match",
+                  %{"player1" => %{"player1_id" => player1_id},
+                  "player1_games" => %{"player1_games" => player1_games},
+                  "player2" => %{"player2_id" => player2_id},
+                  "player2_games" => %{"player2_games" => player2_games}},
+                  socket) do
+    tour = socket.assigns[:tour]
+           |> Repo.preload(:players)
+    Events.assign_match_result(tour, player1_id, player1_games, player2_id, player2_games)
     {:noreply, socket}
   end
 
