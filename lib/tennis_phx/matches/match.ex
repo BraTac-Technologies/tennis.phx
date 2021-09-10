a26ec09ca7ce9b777288bccd35cea65bdaff7372
Yasen Kostov <qskatak04@gmail.com>
2021-09-10 13:10:50 +0300

    refactor match schema model

--- lib/tennis_phx/matches/match.ex
+++ lib/tennis_phx/matches/match.ex
@@ -8,18 +8,23 @@ defmodule TennisPhx.Matches.Match do
   alias TennisPhx.Phases.Phase
   alias TennisPhx.Statuses.Status
   alias TennisPhx.PlayerUnits.Player_unit
+  alias TennisPhx.Matches.Score
 
   schema "matches" do
     belongs_to :tour, Tour
-    belongs_to :player, Player
 
-    field :starting_datetime, :naive_datetime
-    field :score, :map
+
+
+    field :starting_datetime, :date
+    field :player1_id, :integer
+    field :player2_id, :integer
 
     belongs_to :location, Location
     belongs_to :phase, Phase
     belongs_to :status, Status
     belongs_to :player_unit, PlayerUnit
+
+    embeds_one :score, Score, on_replace: :update
 
 
     timestamps()
@@ -28,7 +33,7 @@ defmodule TennisPhx.Matches.Match do
   @doc false
   def changeset(match, attrs) do
     match
-    |> cast(attrs, [:tour_id, :player1_id, :player2_id, :starting_datetime, :score, :location_id, :phase_id, :status_id, :player_unit_id])
-    |> validate_required([:tour_id, :player1_id, :player2_id, :starting_datetime, :score, :location_id, :phase_id, :status_id, :player_unit_id])
+    |> cast(attrs, [:tour_id, :player1_id, :player2_id, :starting_datetime, :location_id, :phase_id, :status_id, :player_unit_id])
+    |> validate_required([:tour_id, :player1_id, :player2_id, :location_id, :phase_id, :player_unit_id])
   end
 end
