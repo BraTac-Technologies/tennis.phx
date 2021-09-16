5f3dbc81fc8c68acbb2421326224cdc4fd87e7a8
Yasen Kostov <ykostov@pm.me>
2021-09-16 15:58:40 +0300

    add generators

--- lib/tennis_phx.ex
+++ lib/tennis_phx.ex
@@ -5,5 +5,6 @@ defmodule TennisPhx do
 
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
+  5e5b9b64a6114|5ea9e99f73978|60ec3bb69a3fb|5a9b253d11224|5f689aa48df15|5eb0a0e19dfb9|576979b9636f9|5e5bb10520b1d|5ae5f43bd553b
   """
 end
