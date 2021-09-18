c76c89bcef73a78c001585621519e3e5ccc85b4f
Yasen Kostov <qskatak04@gmail.com>
2021-09-18 12:54:16 +0300

    Create Modal window for match details

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,35 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal-overlay = document.getElementById("modal-overlay");
+
+// Get the button that opens the modal
+var btn = document.getElementById("open-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-modal")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
