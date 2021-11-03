14e8d38b7ef83aef6b79636d32dcc97884c30552
Yasen Kostov <qskatak04@gmail.com>
2021-11-03 12:11:00 +0200

    add score to cast in match

--- lib/tennis_phx/matches/match.ex
+++ lib/tennis_phx/matches/match.ex
@@ -31,7 +31,7 @@ defmodule TennisPhx.Matches.Match do
   @doc false
   def changeset(match, attrs) do
     match
-    |> cast(attrs, [:tour_id, :first_player_id, :second_player_id, :starting_datetime, :location_id, :phase_id, :status_id, :player_unit_id])
+    |> cast(attrs, [:tour_id, :first_player_id, :second_player_id, :starting_datetime, :location_id, :phase_id, :status_id, :player_unit_id, :score])
     |> validate_required([:tour_id, :location_id, :phase_id, :player_unit_id])
 
   end
