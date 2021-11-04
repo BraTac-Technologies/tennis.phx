916a7aaf3dd56a7ce37da0a51d762cc4b3f58160
Yasen Kostov <qskatak04@gmail.com>
2021-11-04 23:33:45 +0200

    remove score from cast, add cast_embed

--- lib/tennis_phx/matches/match.ex
+++ lib/tennis_phx/matches/match.ex
@@ -31,8 +31,9 @@ defmodule TennisPhx.Matches.Match do
   @doc false
   def changeset(match, attrs) do
     match
-    |> cast(attrs, [:tour_id, :first_player_id, :second_player_id, :starting_datetime, :location_id, :phase_id, :status_id, :player_unit_id, :score])
+    |> cast(attrs, [:tour_id, :first_player_id, :second_player_id, :starting_datetime, :location_id, :phase_id, :status_id, :player_unit_id])
     |> validate_required([:tour_id, :location_id, :phase_id, :player_unit_id])
+    |> cast_embed(:score)
 
   end
 end
