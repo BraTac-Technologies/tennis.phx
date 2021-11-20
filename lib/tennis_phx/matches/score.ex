e0323c12d272c6965e0d6a0f545797eb875e5bc9
Yasen Kostov <qskatak04@gmail.com>
2021-11-20 13:45:28 +0200

    create games 2,3,4,5

--- lib/tennis_phx/matches/score.ex
+++ lib/tennis_phx/matches/score.ex
@@ -6,12 +6,20 @@ defmodule TennisPhx.Matches.Score do
   embedded_schema do
     field :game1_for_first_player, :integer
     field :game1_for_second_player, :integer
+    field :game2_for_first_player, :integer
+    field :game2_for_second_player, :integer
+    field :game3_for_first_player, :integer
+    field :game3_for_second_player, :integer
+    field :game4_for_first_player, :integer
+    field :game4_for_second_player, :integer
+    field :game5_for_first_player, :integer
+    field :game5_for_second_player, :integer
   end
 
   @doc false
   def changeset(score, attrs) do
     score
-    |> cast(attrs, [:game1_for_first_player, :game1_for_second_player])
+    |> cast(attrs, [:game1_for_first_player, :game1_for_second_player, :game2_for_first_player, :game2_for_second_player, :game3_for_first_player, :game3_for_second_player, :game4_for_first_player, :game4_for_second_player, :game5_for_first_player, :game5_for_second_player])
     |> validate_required([])
   end
 end
