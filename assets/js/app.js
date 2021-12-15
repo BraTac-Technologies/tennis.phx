9096b876fc8e65cb92239764ad0a840258141850
Yasen Kostov <ykostov@pm.me>
2021-12-15 11:17:34 +0200

    hotfix js error

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,37 +20,7 @@
 //
 
 // Right click disable
-document.addEventListener('contextmenu', event => event.preventDefault());
 
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-
-// Get the modal
-var modal = document.getElementById("score-modal");
-var modal-overlay = document.getElementById("score-modal-overlay");
-
-// Get the button that opens the modal
-var btn = document.getElementById("score-modal-open");
-
-// Get the <span> element that closes the modal
-var close = document.getElementsById("score-modal-close");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-close.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 import 'alpinejs'
 import "phoenix_html"
