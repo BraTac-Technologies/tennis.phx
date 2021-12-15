68a88eb39014958e86cc18ef03c360a8cdf3b312
Yasen Kostov <ykostov@pm.me>
2021-12-15 15:43:37 +0200

    add Awesomplete autocomplete widget

--- lib/tennis_phx_web/live/head_head_live.ex
+++ lib/tennis_phx_web/live/head_head_live.ex
@@ -5,19 +5,29 @@ defmodule TennisPhxWeb.HeadheadLive do
   alias TennisPhxWeb.PageView
   alias TennisPhx.Events
   alias TennisPhx.Participants
+  alias TennisPhx.Matches
+  alias TennisPhx.Matches.Match
 
   def render(assigns) do
    render PageView, "headhead.html", assigns
   end
 
   def mount(params, _, socket) do
-    tours = Events.list_tours() |> Repo.preload(:players)
-    players = Participants.list_players_ranking() |> Repo.preload(:tours)
+    tours = Events.list_tours()
+    players = Participants.list_players()
+    changeset = Matches.change_match(%Match{})
     socket = assign(
         socket,
         tours: tours,
-        players: players
+        players: players,
+        changeset: changeset
       )
     {:ok, socket}
+  end
+
+  def handle_event("suggest-player", %{"player1" => prefix}, socket) do
+    socket = assign(socket, matches: Participants.suggest(prefix))
+    {:noreply, socket}
+
   end
 end
