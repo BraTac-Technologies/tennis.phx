59f088ff080840535dc56b122479fc2493586d22
Yasen Kostov <qskatak04@gmail.com>
2022-01-31 21:14:12 +0200

    handle event h2h search by players' id

--- lib/tennis_phx_web/live/headtohead.ex
+++ lib/tennis_phx_web/live/headtohead.ex
@@ -4,6 +4,8 @@ defmodule TennisPhxWeb.HeadtoHeadLive do
   alias TennisPhx.Repo
   alias TennisPhxWeb.PageView
   alias TennisPhx.Participants
+  alias TennisPhx.Matches
+  alias TennisPhx.Matches.Match
 
   def render(assigns) do
    render PageView, "headtohead.html", assigns
@@ -12,18 +14,36 @@ defmodule TennisPhxWeb.HeadtoHeadLive do
 
   @impl true
   def mount(params, _, socket) do
-    id = 1
-    player = Participants.get_player!(id)
-    winrate = Participants.get_winrate(player)
-    match_count = Participants.get_match_count(player)
-
+    changeset = Matches.change_match(%Match{})
+    players = Participants.list_players()
     socket = assign(
         socket,
-        player: player,
-        winrate: winrate,
-        match_count: match_count
+        matches: [],
+        first_player: nil,
+        second_player: nil,
+        changeset: changeset
       )
     {:ok, socket}
+  end
+
+  def handle_event("search_for_matches", %{
+                  "player1" => %{"player1" => player1},
+                  "player2" => %{"player2" => player2}},
+                  socket) do
+
+    first_player = Participants.get_player!(player1)
+    second_player = Participants.get_player!(player2)
+    matches = Matches.get_match_by_players(player1, player2) |> Repo.preload(:first_player) |> Repo.preload(:second_player) |> Repo.preload(:phase) |> Repo.preload(:tour)
+
+    socket = assign(
+    socket,
+    matches: matches,
+    first_player: first_player,
+    second_player: second_player
+    )
+
+    {:noreply, socket}
+
   end
 
 end
